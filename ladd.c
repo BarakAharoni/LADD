f727e5783464d141778cad661c082832f0f4106f
Barak Aharoni <97598628+BarakAharoni@users.noreply.github.com>
2022-06-08 17:46:22 +0300

    Update ladd.c

--- ladd.c
+++ ladd.c
@@ -1,3 +1,22 @@
+/*
+
+################################################################
+#                                                              #
+#                                                              #
+#     _____          _       ______   ______                   #
+#    |_   _|        / \     |_   _ `.|_   _ `.                 #
+#      | |         / _ \      | | `. \ | | `. \                #
+#      | |   _    / ___ \     | |  | | | |  | |                #
+#     _| |__/ | _/ /   \ \_  _| |_.' /_| |_.' /                #
+#    |________||____| |____||______.'|______.'                 #
+#                                                              #
+#    Linux Anti-Debugging Detector                             #
+#                                                              #
+#    Copyright (c) 2022 Barak Aharoni.  All Rights Reserved.   #
+################################################################
+
+*/
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -15,7 +34,7 @@ inline void detectTracerPID();
 inline void detectLD_PREALOAD();
 inline void detectPtrace();
 
-
+// Get process name by its PID
 char* getProcnameByPID(int pid)
 {
     char* name = (char*)calloc(1024, sizeof(char));
@@ -37,6 +56,7 @@ char* getProcnameByPID(int pid)
     return name;
 }
 
+// Reads the /proc/self/status file and TracerPid field to detect an attached debugger
 void detectTracerPID()
 {
     printf("TracerPID Check\n");
@@ -49,12 +69,15 @@ void detectTracerPID()
     char* content;
 
     fptr = fopen(PROC_STATUS_PATH, "r");
+    
+    // Cannot open the file
     if(fptr == NULL)
     {
         printf("\t[-] Error opening: %s\n", PROC_STATUS_PATH);
         exit(1);
     }
 
+    // Reads every line in the file until finding the 'TracerPid' field
     while(fgets(line, lineLength, fptr)) 
     {
         content = strstr(line, "TracerPid");
@@ -63,27 +86,38 @@ void detectTracerPID()
     }
     fclose(fptr);
 
+    // Use sscanf to catch the PID of the debugger process
     int ret = sscanf(content, "%s %d" , tracer, &tracerPid);
+    
+    // The current process is being debugged
     if (tracerPid != NOT_DEBUGGED_TRACERPID)
         printf("\t[V] The process is being Debugged by PID: %d, ProcessName: %s\n", tracerPid, getProcnameByPID(tracerPid));
+    
+    // The current process is not debugged
     else
         printf("\t[X] The process is NOT Debugged\n");
         
 }
 
+// Checks the LD_PRELOAD environment variable
 void detectLD_PREALOAD()
 {
     printf("LD_PREALOAD Check\n");
     const char* ldEnvar = getenv(LD_PRELOAD);
+    
+    // LD_PRELOAD environment variable is empty
     if(ldEnvar != NULL)
         printf("\t[V] %s environment variable found: %s\n", LD_PRELOAD, ldEnvar);
     else
         printf("\t[X] %s environment variable not found\n", LD_PRELOAD);
 }
 
+// Use the PTRACE_TRACEME Syscall to detect an attached debugger
 void detectPtrace()
 {
     printf("Ptrace Check\n");
+    
+    // PTRACE_TRACEME Syscall is already in used
     if (ptrace(PTRACE_TRACEME, 0, 1, 0) == DEBUGGER_PRESENT) 
         printf("\t[V] Process is being debugged\n");
     else
